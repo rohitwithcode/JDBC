1e62afd4bfb72e4f3e5d96ee4070e57d030b20a6
rohitwithcode <radharani7253@gmail.com>
2023-10-18 21:05:46 +0530

    Retrieve_and_Select_data_from_database added

--- src/Retrieve_and_Select_data_from_database.java
+++ src/Retrieve_and_Select_data_from_database.java
@@ -9,6 +9,7 @@ public class Retrieve_and_Select_data_from_database {
     public static void main(String[] args) {
 
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","CoderRohit","Radhey@725386");
